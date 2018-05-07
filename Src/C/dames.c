4e240e84ca49d6c5d183892a930cf54fe76417a2
yakobibus <31994217+yakobibus@users.noreply.github.com>
2018-05-08 00:28:18 +0200

    Plateau
    Debut implémentation

--- Src/C/dames.c
+++ Src/C/dames.c
@@ -2,10 +2,18 @@
 
 # include <iostream>
 # include "dames.h"
+# include "plateau.h"
 
 int main (int argc, char** argv)
 {
     int retCode = 0;
+
+    spc_plateau::Joueur joueur1;
+    spc_plateau::Joueur joueur2;
+    /*
+    spc_plateau::Plateau plateau(joueur1, joueur2) ;
+    */
+   spc_plateau::Dummy d ;
 
     return retCode ;
 }
