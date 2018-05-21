2e2e70e23b835a5afcbcaae2ab4b79f292940a70
yakobibus <31994217+yakobibus@users.noreply.github.com>
2018-05-21 22:31:23 +0200

    Commentaire sur une règle

--- Src/H/regles.h
+++ Src/H/regles.h
@@ -4,6 +4,10 @@
   # define _REGLES_H_ (1)
   namespace spc_regles
   {
+// Movement - Regle 1 : La case de départ doit être occupée par un pion de la même couleur que le joueur
+//            Règle 2 : La case destination pour un déplacement, une prise, une étape de raffle doit être libre
+//            Règle 3 : Pour un déplacement (pion), la case destination doit être voisine de la case de départ en diagonale vers l'avant par rapport à la couleur en jeu
+//            Règle 4 : Pour un déplacement (reine), la case destination doit être dans la diagonale et aucun pion ne doit se trouver entre les deux
 	  class Regle
 	  {
 	  public :
