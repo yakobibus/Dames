0c37471db21868a00bb06ec9fd1a95e4776aca81
yakobibus <yakobibus@live.fr>
2019-12-06 18:05:21 +0100

    Validation du Couop.départ ...
    ... instable ...

--- Src/C/coup.cpp
+++ Src/C/coup.cpp
@@ -4,7 +4,20 @@
 
 namespace spc_dames
 {
-	void Coup::jouer(unsigned& id, Joueur* joueur) 
+	bool Coup::isValidDepart(void)
+	{
+		if (_celluleDepart->getPionAddress() == nullptr) { _eMoveErrors = eMoveErrors::emptyStartingCell; } // Cellule départ occupée ?
+		if (_joueur->eCouleur() == _celluleDepart->getPionAddress()->eCouleur()) { _eMoveErrors |= eMoveErrors::colorsMismatch; }
+		_isValidDepart = (
+			_eMoveErrors == eMoveErrors::noErrors
+			&&   // Le joueur avance un pion de sa couleur : implique 
+			&& true // Le pion départ est libre (ou peut sauter) 
+			);
+		
+		return _isValidDepart;
+	}
+
+	void Coup::jouer(unsigned& id, Joueur* joueur)
 	{
 		_id = id;
 		_joueur = joueur;
--- Src/H/coup.h
+++ Src/H/coup.h
@@ -10,26 +10,39 @@
 
 namespace spc_dames
 {
+	enum class eMoveErrors
+	{
+		noErrors = 0
+		, emptyStartingCell = 1
+		, colorsMismatch = 2
+		, zz = 4
+	};
+}
+
+namespace spc_dames
+{
 	class Coup
 	{
 	public :
-		Coup() : _id(0), _joueur(nullptr), _pion(nullptr), _celluleDepart(nullptr), _celluleArrivee(nullptr) {}
+		Coup() : _id(0), _joueur(nullptr), _pion(nullptr), _celluleDepart(nullptr), _celluleArrivee(nullptr), _eMoveErrors(eMoveErrors::noErrors), _isValidDepart(false) {}
 		Coup(const unsigned& id, Joueur* joueurAddress, Pion* pionAddress, Cellule* scramblingCellAddress, Cellule* fallingCellAddress) 
-			: _id(id), _joueur(joueurAddress), _pion(pionAddress), _celluleDepart(scramblingCellAddress), _celluleArrivee(fallingCellAddress) {}
+			: _id(id), _joueur(joueurAddress), _pion(pionAddress), _celluleDepart(scramblingCellAddress), _celluleArrivee(fallingCellAddress), _eMoveErrors(eMoveErrors::noErrors), _isValidDepart(false) {}
 		~Coup() = default;
 		Coup(const Coup& c) = default;
 		Coup& operator = (const Coup& c) = default;
 		//
 		//bool isValidDepart(void) { std::cout << "Joueur " <<_joueur->couleurTexte()<<", pion "<<_celluleDepart->getMotifPion()<<"). \n"; return false; }
-		bool isValidDepart(void) { std::cout << "Joueur " << _joueur->couleurTexte() << ", pion " << _celluleDepart->getMotifPion() << " (" << (_joueur->eCouleur() == _celluleDepart->getPionAddress()->eCouleur() ? "true" : "false") << "). \n"; return false; }
+		bool isValidDepart(void);
 		void jouer(unsigned& id, Joueur* joueur);
 	private :
 		//Input     _input; /// TODO : ici
-		unsigned  _id;
-		Joueur*   _joueur ;
-		Pion*     _pion ;
-		Cellule*  _celluleDepart ;
-		Cellule*  _celluleArrivee ;
+		unsigned    _id;
+		Joueur*     _joueur ;
+		Pion*       _pion ;
+		Cellule*    _celluleDepart ;
+		Cellule*    _celluleArrivee ;
+		eMoveErrors _eMoveErrors;
+		bool        _isValidDepart;
 	};
 }
 
