6a8750db9f595308c381781f57bd3723bab5a314
yakobibus <31994217+yakobibus@users.noreply.github.com>
2019-12-29 00:04:05 +0100

    En cours de dev ...
    ... sur les diagonales

--- Src/C/coordonnees.cpp
+++ Src/C/coordonnees.cpp
@@ -7,12 +7,10 @@ namespace spc_dames
 {
 	void Coordonnees::set(const YXM& yxm, const Manoury* manouryAddress)
 	{
-		_yxmAdress = &yxm;
+		_yxmAddress = &yxm;
 		_manouryAddress = manouryAddress;
-		//_diagonales = _yxmAdress->manoury.
-		//Manoury m;
-		_manouryAddress->getNeighbours(_manouryVoisines, _yxmAdress->manoury);
-		//_manouryVoisines = _yxmAdress->manoury.getNeighbours(_manouryVoisines, _yxmAdress->manoury);
+		_diagonales = _manouryAddress->getDiagonales(_yxmAddress->manoury); TODO: ajouter une fonction qui retourne un vector des Diagonales de la Manoury reçue
+		_manouryAddress->getNeighbours(_manouryVoisines, _yxmAddress->manoury);
 		//_adresseMotif = adresseMotif; 
 	}
 }
--- Src/H/coordonnees.h
+++ Src/H/coordonnees.h
@@ -11,21 +11,21 @@ namespace spc_dames
 	class Coordonnees
 	{
 	public:
-		Coordonnees() : _yxmAdress(nullptr), _manouryAddress(nullptr) {}
-		Coordonnees(YXM* yxm, Manoury* manouryAddress, char* motif) : _yxmAdress(yxm), _manouryAddress(manouryAddress) {}
+		Coordonnees() : _yxmAddress(nullptr), _manouryAddress(nullptr) {}
+		Coordonnees(YXM* yxm, Manoury* manouryAddress, char* motif) : _yxmAddress(yxm), _manouryAddress(manouryAddress) {}
 		~Coordonnees() = default;
 		Coordonnees(const Coordonnees& c) = default;
 		Coordonnees& operator = (const Coordonnees& c) = default;
 		// ---
 		void set(const YXM& yxm, const Manoury* manouryAddress);
 		Coordonnees& get(void) { return *this; }
-		unsigned int getX(void) const { return _yxmAdress->yx.x; }
-		unsigned int getY(void) const { return _yxmAdress->yx.y; }
-		const YXM*         getXMaddress(void) const { return _yxmAdress; }
-		unsigned int getManoury(void) const { return _yxmAdress->manoury; } 
-		char         getXalpha(void) const { return 'a' + _yxmAdress->yx.x - 1; }
+		unsigned int getX(void) const { return _yxmAddress->yx.x; }
+		unsigned int getY(void) const { return _yxmAddress->yx.y; }
+		const YXM*         getXMaddress(void) const { return _yxmAddress; }
+		unsigned int getManoury(void) const { return _yxmAddress->manoury; } 
+		char         getXalpha(void) const { return 'a' + _yxmAddress->yx.x - 1; }
 	private:
-		const YXM*                         _yxmAdress ;
+		const YXM*                         _yxmAddress ;
 		const Manoury*                     _manouryAddress;
 		std::vector<std::vector<unsigned>> _diagonales ;
 		std::vector<unsigned>              _manouryVoisines ;
